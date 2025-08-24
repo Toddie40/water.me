c99100aeefafe529087ff4536b2d4a4bed616ceb
Alex Todd <Alex.Todd40@gmail.com>
2025-08-24 15:08:40 +0100

    updaed log endpoint to use database_connection() defined in utils/db_conf

--- src/api/fastapi_app/log.py
+++ src/api/fastapi_app/log.py
@@ -1,9 +1,6 @@
 # utility to hold the functions to log requests to the database
-from fastapi import Request
-from pydantic import BaseModel, Json
-from typing import Any
-import os
-import psycopg2
+from pydantic import BaseModel
+from .utils.db_conf import database_connection
 import datetime
 
 
@@ -19,11 +16,6 @@ class LogEntry(BaseModel):
     client_ip: str
 
 def log(entry: LogEntry):
-    db_host = 'database'
-    db_user = os.environ.get('POSTGRES_USER', 'postgres')
-    db_password = os.environ.get('POSTGRES_PASSWORD', 'postgres')
-    db_name = os.environ.get('POSTGRES_DB', 'postgres')
-
     # generate sql query string
     query = f"""
     INSERT INTO log (timestamp, endpoint, method, request_body, response_status, response_body, client_ip)
@@ -32,13 +24,7 @@ def log(entry: LogEntry):
 
     # make db connection
     try:
-        with psycopg2.connect(
-            host=db_host,
-            port=5432,
-            dbname=db_name,
-            user=db_user,
-            password=db_password
-        ) as conn:
+        with database_connection() as conn:
             with conn.cursor() as curs:
                 curs.execute(query, 
                             {
