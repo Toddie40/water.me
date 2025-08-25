64e2d9c47fe136777bfeeb96bc945c7a4ab9c902
Alex Todd <Alex.Todd40@gmail.com>
2025-08-26 00:11:17 +0100

    removed navabr from global css and tidied navbar component away with its own css file.

--- src/frontend/app/components/navbar/Navbar.tsx
+++ src/frontend/app/components/navbar/Navbar.tsx
@@ -3,18 +3,19 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import './Navbar.css'
 
 const NavigationBar = () => {
     return (
         <Navbar expand="lg" className="navbar">
       <Container>
-        <Navbar.Brand href="/">water.me</Navbar.Brand>
+        <Navbar.Brand href="/"><div className='navbarTitle'>water.me</div></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="/">Status</Nav.Link>
-            <Nav.Link href="library">Plants Library</Nav.Link>
-            <Nav.Link href="logs">Logs</Nav.Link>
+            <Nav.Link href="/"><div className='navbarLink'>Status</div></Nav.Link>
+            <Nav.Link href="library"><div className='navbarLink'>Library</div></Nav.Link>
+            <Nav.Link href="logs"><div className='navbarLink'>Logs</div></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
