b1682e72405611c6e98bd2c0d99a42c07c2448be
Alex Todd <Alex.Todd40@gmail.com>
2025-08-26 00:10:27 +0100

    swapped bootstrap and global css order for correct behaviour

--- src/frontend/app/layout.tsx
+++ src/frontend/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import "./globals.css";
 
-import Navbar from './components/Navbar';
+import Navbar from './components/navbar/Navbar';
 import Footer from "./components/Footer";
 
 const geistSans = Geist({
